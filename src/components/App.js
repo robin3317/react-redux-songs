c984b8424cb557ecfa0f8ba504b43185c54dc418
robin3317 <abdurrahmanrobin47@gmail.com>
2019-02-19 02:22:50 +0600

    Show songs from redux store

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,12 @@ import { selectSong } from '../actions';
 
 const App = () => {
   return(
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+
+  renderList = () => {
+    return this.props.songs.map(song => {
+      return(
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props);
     return(
-      <div>SongList</div>
+      <div className="ui divided list">{this.renderList()}</div>
     );
   }
 }
